f4420744620a70a36f0ef3c5e7867cad1194c55a
Jonathan Reem <jonathan.reem@gmail.com>
2014-07-04 19:12:06 -0700

    (test) Added tests.

--- tests/index.js
+++ tests/index.js
@@ -5,4 +5,24 @@ describe('shield-func', function () {
   it('should exist', function () {
     demand(shield).to.exist();
   });
+
+  describe('when wrapping a function', function () {
+    it('should return a function', function () {
+      demand(shield(function () {})).to.be.a(Function);
+    });
+
+    describe('the returned function', function () {
+      it('should call the passed-in function', function () {
+        var called = false;
+        var spy = function () { called = true; };
+        shield(spy)();
+        demand(called).to.be.true();
+      });
+
+      it('should call the passed-in function with no arguments', function () {
+        var spy = function () { demand(arguments.length).to.equal(0); };
+        shield(spy)(1, 2, 3, 4);
+      });
+    });
+  });
 });
